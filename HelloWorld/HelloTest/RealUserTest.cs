d0da8cde6f533e7d22c8750cbaaa197db2de9aae
fanny <fanny.miech@le-campus-numerique.fr>
2021-02-03 09:57:54 +0100

    add test for RealUser

---
+++ HelloWorld/HelloTest/RealUserTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using HelloWorld;
+
+namespace HelloTest
+{
+    [TestClass]
+    public class RealUserTest
+    {
+        [TestMethod]
+        public void TestGetNameReturnsCorrectName()
+        {
+            //Arrange
+            IUser user = new RealUser();
+            //Act
+            string actualUserName = user.Name;
+            //Assert
+            string expectedUserName = Environment.UserName;
+            Assert.AreEqual(actualUserName, expectedUserName);
+        }
+    }
+}
